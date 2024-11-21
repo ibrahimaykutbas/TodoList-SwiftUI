78397c1275ead500ec5de6b5af339906cf39a5fb
İbrahim Aykut BAŞ <ibrahimaykutbas@gmail.com>
2024-11-21 20:01:22 +0300

    feat: Project created

---
+++ TodoList/TodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListApp.swift
+//  TodoList
+//
+//  Created by İbrahim Aykut BAŞ on 21.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct TodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
